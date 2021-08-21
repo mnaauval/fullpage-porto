1ed170befd0e3908ceb8583d65ba30739874fbe2
mnaauval <mnaauval20@gmail.com>
2021-08-21 18:40:31 +0700

    update active nav scrolling

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,7 @@
 // FullPage
 new fullpage("#fullpage", {
   anchors: ["page1", "page2", "page3", "page4", "page5"],
-  mene: "#navbarNav",
+  menu: "#navbarNav",
   scrollOverflow: true,
   responsiveWidth: 768,
   responsiveHeight: 600,
@@ -16,25 +16,6 @@ new fullpage("#fullpage", {
 //   activeNavItem.removeClass("active");
 //   $(this).addClass("active");
 // });
-
-// One Page Nav jquery
-$("#myNav").onePageNav({
-  currentClass: "current",
-  changeHash: false,
-  scrollSpeed: 750,
-  scrollThreshold: 0.5,
-  filter: "",
-  easing: "swing",
-  begin: function () {
-    //I get fired when the animation is starting
-  },
-  end: function () {
-    //I get fired when the animation is ending
-  },
-  scrollChange: function ($currentListItem) {
-    //I get fired when you enter a section and I pass the list item of the section
-  },
-});
 
 // owl-carousel
 $(document).ready(function () {
